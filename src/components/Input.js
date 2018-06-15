b7b3c9b700ab573bd04cf942a52e96f868afdf61
Jason Martin <jmartin@popsugar.com>
2018-06-15 16:35:14 -0700

    Test deploy

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default (props) => (
   <div>
+      Input:
     <input type="file" accept="image/*" capture="camera" onChange={() => alert('I changed')} />
   </div>
 );
