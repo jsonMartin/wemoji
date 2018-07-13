688dc04a255b5c043af92ac171e81f1235b99614
Jason Martin <jmartin@popsugar.com>
2018-07-13 16:24:18 -0700

    Refactor fetchFaceData action creator

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import fetchFaceData from '../actions/fetchFaceData.js';
 
 const Canvas = styled.canvas`
   width: 100vw;
@@ -15,49 +12,55 @@ class Photo extends React.Component {
   state = {}
 
   async componentDidMount() {
-    const { image, fetchFaceData } = this.props;
-    this.drawImageToCanvas(image);
-    fetchFaceData(image);
-  }
-
-  componentDidUpdate() {
     const { image } = this.props;
     this.drawImageToCanvas(image);
   }
 
-  drawImageToCanvas() {
+  componentDidUpdate() {
     const { image, faceData } = this.props;
-    // const { image } = this.props;
+    this.drawImageToCanvas(image);
+    if (Array.isArray(faceData)) this.drawFaceData(); // Draw Face Data, if it exists
+  }
+
+  get canvasContext() {
+    return this.canvas.getContext('2d');
+  }
+
+  get canvas() {
+    return this.canvasRef.current;
+  }
+
+  drawImageToCanvas() {
+    const { image } = this.props;
     const canvas = this.canvasRef.current;
     canvas.width = image.canvas.width;
     canvas.height = image.canvas.height;
-    // debugger;
-    const context = canvas.getContext('2d');
+    const context = this.canvasContext;
     context.imageSmoothingEnabled = false; // Anti-Aliasing messes up image render drawing
     console.log('Drawing image on canvas');
-    // context.drawImage(image.canvas, 0.5, 0.5, canvas.width, canvas.height);
-    // context.drawImage(image.canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
     context.drawImage(image.canvas, 0, 0);
-    if (Array.isArray(faceData)) {
-      if (faceData.length === 0) return this.drawNoFacesDetectedText();
-      for (const face of faceData) {
-        this.drawFaceRectangleBoxes(face);
-      }
+  }
+
+  drawFaceData() {
+    console.log('Drawing face data on canvas');
+    const { faceData } = this.props;
+
+    if (faceData.length === 0) return this.drawNoFacesDetectedText();
+    for (const face of faceData) {
+      this.drawFaceRectangleBoxes(face);
     }
   }
 
   drawNoFacesDetectedText() {
-    const canvas = this.canvasRef.current;
-    const context = canvas.getContext('2d');
+    const context = this.canvasContext;
     context.font = '30px Comic Sans MS';
     context.fillStyle = 'red';
     context.textAlign = 'center';
-    context.fillText('No face detected', canvas.width / 2, canvas.height / 2);
+    context.fillText('No face detected', this.canvas.width / 2, this.canvas.height / 2);
   }
 
   drawFaceRectangleBoxes({ faceRectangle }) {
-    const canvas = this.canvasRef.current;
-    const context = canvas.getContext('2d');
+    const context = this.canvasContext;
     context.lineWidth = '6';
     context.strokeStyle = 'red';
     context.rect(faceRectangle.left, faceRectangle.top, faceRectangle.width, faceRectangle.height);
@@ -78,4 +81,4 @@ function mapStateToProps(state) {
 
   return { image, faceData };
 }
-export default connect(mapStateToProps, dispatch => bindActionCreators({ fetchFaceData }, dispatch))(Photo);
+export default connect(mapStateToProps, null)(Photo);
