9276fb3c856fb9962addc1deb54ce27f6d13b2b5
Jason Martin <jmartin@popsugar.com>
2018-08-14 16:47:54 -0700

    Remove console logs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ export default combineReducers({
   image: (state = null, action) => { // Change to capturedImage
     switch (action.type) {
       case 'CAMERA_BUTTON_PRESSED':
-        console.log('Camera button pressed reducer fired');
         return action.payload;
       default:
         return state;
@@ -13,10 +12,8 @@ export default combineReducers({
   showModal: (state = false, action) => {
     switch (action.type) {
       case 'DISMISS_MODAL':
-        console.log('DISSMISS_MODAL RECEIVED!!!!!!!');
         return false;
       case 'CAMERA_BUTTON_PRESSED':
-        console.log('Show modal reducer fired');
         return true;
       default:
         return state;
